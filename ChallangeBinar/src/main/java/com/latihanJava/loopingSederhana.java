a1e0460736085f0732969a4d1e27588f2ab95283
AriSatriaWiratama <arisatriawiratama1@gmail.com>
2023-12-18 12:28:43 +0700

    Loping Sederhana

---
+++ ChallangeBinar/src/main/java/com/latihanJava/loopingSederhana.java
@@ -0,0 +1,22 @@
+package com.latihanJava;
+
+import java.util.Scanner;
+
+public class loopingSederhana {
+    public static void main (String[] args){
+        Scanner inputUser = new Scanner(System.in);
+        int hasilTotal,awalUser,akhirUser;
+        System.out.printf("masukan nilai awal = ");
+        awalUser = inputUser.nextInt();
+        System.out.printf("masukan nilai akhir = ");
+        akhirUser = inputUser.nextInt();
+
+        hasilTotal= 0;
+
+        while (awalUser <= akhirUser){
+            hasilTotal = hasilTotal+awalUser;
+            System.out.printf("bilangan ke =%d hasil totalnya adalah = %d\n",awalUser,hasilTotal);
+            awalUser++;
+        }
+    }
+}
