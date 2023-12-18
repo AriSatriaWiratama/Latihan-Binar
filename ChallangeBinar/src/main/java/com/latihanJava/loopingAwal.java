75ebdce1da074b898ef204adb062efbee6d22bc5
AriSatriaWiratama <arisatriawiratama1@gmail.com>
2023-12-18 12:27:12 +0700

    belajar Looping

---
+++ ChallangeBinar/src/main/java/com/latihanJava/loopingAwal.java
@@ -0,0 +1,15 @@
+package com.latihanJava;
+
+
+public class loopingAwal {
+
+    public static void main(String[] args) {
+        System.out.println("membuat looping for = ");
+        for (int a = 0; a <= 10; a++){
+            System.out.printf("nilainya adalah %d \n",a);
+        }
+
+        System.out.println("membuat looping for = minus");
+    }
+
+}
