7703f675f1f6b9f6f9d64b166aa931071f70b3ac
AriSatriaWiratama <arisatriawiratama1@gmail.com>
2023-12-20 20:34:12 +0700

    Array Looping

---
+++ ChallangeBinar/src/main/java/com/latihanJava/arrayLooping.java
@@ -0,0 +1,29 @@
+package com.latihanJava;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class arrayLooping {
+    public static void main(String[] args){
+        int[] arrayData = {10,11,12,13,14,15,16,17};
+        //list
+        List<String> listString = new ArrayList<>();
+        listString.add("GG GAMING");
+
+        System.out.println(listString.get(0));
+
+        System.out.println("panjang data array = "+ arrayData.length);
+        System.out.println("Looping dengan properti");
+        for (int i= 0; i < arrayData.length; i++){
+            System.out.print(arrayData[i]+" ");
+        }
+
+        System.out.println();
+
+        System.out.println("Looping data");
+        // fitur ini dinamakan For Each (:)
+        for (int angka : arrayData){
+            System.out.print(angka+" ");
+        }
+    }
+}
