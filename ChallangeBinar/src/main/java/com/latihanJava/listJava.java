178252dc65bed60f0b95b2ac50f11cc8dd21596d
AriSatriaWiratama <arisatriawiratama1@gmail.com>
2023-12-21 13:56:56 +0700

    List pada java

---
+++ ChallangeBinar/src/main/java/com/latihanJava/listJava.java
@@ -0,0 +1,13 @@
+package com.latihanJava;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class listJava {
+    public static void main(String[] args){
+        List<String> listString = new ArrayList<>();
+        listString.add("GG GAMING");
+
+        System.out.println(listString.get(0));
+    }
+}
