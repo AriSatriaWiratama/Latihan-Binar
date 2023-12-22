9d42b005090cd2ac3ce92f6df635f9a51aa928be
Ari Satria Wiratama <arisatriawiratama1@gmail.com>
2023-12-22 20:11:01 +0700

    java oop

---
+++ ChallangeBinar/src/main/java/com/latihanJava/JavaOOP.java
@@ -0,0 +1,24 @@
+package com.latihanJava;
+
+public class JavaOOP {
+    public static void main(String[] args){
+
+    }
+    private String nama;
+    private String gaming;
+
+    private void setNama(String nama){
+        this.nama = nama;
+    }
+    private String getNama(){
+        return this.nama;
+    }
+
+    private void setGaming(String gaming){
+        this.gaming = gaming;
+    }
+
+    private String getGaming(){
+        return this.gaming;
+    }
+}
