88c28756ced9105978fccacd7395c72638cd375d
Ari Satria Wiratama <arisatriawiratama1@gmail.com>
2023-12-27 13:20:40 +0700

    Menambahkan data dosen

--- ChallangeBinar/src/main/java/oriented/MahasiswaMain.java
+++ ChallangeBinar/src/main/java/oriented/MahasiswaMain.java
@@ -2,16 +2,29 @@ package oriented;
 
 public class MahasiswaMain {
     public static void main(String[] args) throws Exception{
-        Mahasiswa dosen1 = new Mahasiswa();
-        dosen1.nama = "Ari Satria Wiratama";
-        dosen1.nim = "41519010122";
-        dosen1.jurusan = "Teknik Informatika";
-        dosen1.umur = 22;
+        Mahasiswa mahasiswa1 = new Mahasiswa();
+        mahasiswa1.nama = "Ari Satria Wiratama";
+        mahasiswa1.nim = "41519010122";
+        mahasiswa1.jurusan = "Teknik Informatika";
+        mahasiswa1.umur = 22;
 
 
-        System.out.println(dosen1.nama);
-        System.out.println(dosen1.nim);
-        System.out.println(dosen1.jurusan);
-        System.out.println(dosen1.umur);
+        System.out.println(mahasiswa1.nama);
+        System.out.println(mahasiswa1.nim);
+        System.out.println(mahasiswa1.jurusan);
+        System.out.println(mahasiswa1.umur);
+
+        //membuat object dengan memasukan constructor didalam class
+        Dosen dosen1 = new Dosen("Bagus Dwi","10101010","Teknik informatika");
+
+        dosen1.show();
+        dosen1.setNama("Titanius Dwi");
+        dosen1.show();
+
+        System.out.println(dosen1.getNama());
+
+        String data = dosen1.halo("gg gaming");
+        System.out.println(dosen1.halo("beliau"));
+        System.out.println(data);
     }
 }
