c69a032131f13635a9b88d7eb74e926b725fafdd
Ari Satria Wiratama <arisatriawiratama1@gmail.com>
2023-12-27 13:20:29 +0700

    Membuat class dosen oop

---
+++ ChallangeBinar/src/main/java/oriented/Dosen.java
@@ -0,0 +1,40 @@
+package oriented;
+
+public class Dosen {
+    String nama;
+    String nim;
+
+    String jurusan;
+
+    Dosen(String nama,String nim,String jurusan){
+        this.nama=nama;
+        this.nim=nim;
+        this.jurusan=jurusan;
+
+        System.out.println(nama);
+        System.out.println(nim);
+        System.out.println(jurusan);
+    }
+
+    // Method tanpa return dan tanpa parameter
+    void show(){
+        System.out.println("Nama :"+this.nama);
+        System.out.println("Nim :"+this.nim);
+        System.out.println("Jurusan :"+this.jurusan);
+    }
+
+    // Method tanpa return dan dengan parameter
+    void setNama(String nama){
+        this.nama =nama;
+    }
+
+    // Method dengan return dan tanpa parameter
+    String getNama(){
+        return this.nama;
+    }
+
+    String halo(String pesan){
+        return pesan +"ini punya"+this.nama;
+    }
+
+}
